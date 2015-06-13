a2c182dda0a11a8542c7515a15f1cc48b674396a
Miguel Fernández-Vázquez Crespo <mifercre@gmail.com>
2015-06-13 14:10:39 +0200

    Navigation drawer added

---
+++ ShipeerAndroid/Shipeer/app/src/main/java/async/interfaces/OnDeleteOwnTripTaskCompleted.java
@@ -0,0 +1,8 @@
+package async.interfaces;
+
+/**
+ * Created by mifercre on 01/06/15.
+ */
+public interface OnDeleteOwnTripTaskCompleted {
+    void onDeleteOwnTripTaskCompleted(String[] result);
+}
